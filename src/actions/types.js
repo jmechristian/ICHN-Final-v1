d0c3d1a18064e43dccacbbf4d47a2d53473d933f
Jim Eiler <jimboaggie@gmail.com>
2019-03-14 19:11:59 -0500

    Jim changes

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,5 +1,7 @@
-export const GET_ERRORS = 'GET_ERRORS';
-export const CLEAR_ERRORS = 'CLEAR_ERRORS';
-export const SET_CURRENT_USER = 'SET_CURRENT_USER';
-export const GET_ORGS = 'GET_ORGS';
-export const SET_FOLLOWING = 'SET_FOLLOWING';
+export const GET_ERRORS = "GET_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const GET_ORGS = "GET_ORGS";
+export const SET_FOLLOWING = "SET_FOLLOWING";
+
+export const JIM = "Jim Eiler";
