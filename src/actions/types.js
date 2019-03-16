0ee8d0c04cf7b7f4bb6e1e06b634c4c86d317e44
Jim Eiler <jimboaggie@gmail.com>
2019-03-16 13:16:38 -0500

    jimbo test

--- src/actions/types.js
+++ src/actions/types.js
@@ -4,4 +4,4 @@ export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const GET_ORGS = "GET_ORGS";
 export const SET_FOLLOWING = "SET_FOLLOWING";
 
-export const JIM = "Jim Eiler";
+export const JIM = "Jimbo Eiler";
