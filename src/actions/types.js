bee0cb4e678ce9e3ad2abb4c476a4ff058c62573
Jim Eiler <jimboaggie@gmail.com>
2019-03-16 16:04:24 -0500

    removed me

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,9 +1,7 @@
-export const GET_ERRORS = 'GET_ERRORS';
-export const CLEAR_ERRORS = 'CLEAR_ERRORS';
-export const SET_CURRENT_USER = 'SET_CURRENT_USER';
-export const GET_ORGS = 'GET_ORGS';
-export const SET_FOLLOWING = 'SET_FOLLOWING';
+export const GET_ERRORS = "GET_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const GET_ORGS = "GET_ORGS";
+export const SET_FOLLOWING = "SET_FOLLOWING";
 
-export const JIM = 'Jimbo Eiler';
-export const STEPHEN = 'STEPHEN';
-export const JAMIE = 'Jamie';
+export const JAMIE = "Jamie";
